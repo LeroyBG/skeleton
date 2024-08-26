0e9e905d9fef339584fccbdd1dd466a33e7e8b45
LeroyBG <leroylightning@g.ucla.edu>
2024-08-26 00:04:17 -0700

    tinkering

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -8,7 +8,7 @@
         href={PUBLIC_PROJECT_GITHUB_LINK}
         target="_blank">run it locally</a> :)</p>
 </header>
-<div class="flex flex-col items-center gap-32">
+<div class="flex flex-col items-center gap-5 lg:gap-32">
     <div id="top-title-container" class="flex w-full justify-center items-center text-7xl mt-5 text-irishJig-300">
         <h1 id="title" class="text-center">skeleton</h1>
     </div>
