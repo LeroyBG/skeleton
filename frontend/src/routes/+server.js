f9520f3913030b5c19d3f7fc33c24cf3e8e41f66
LeroyBG <leroylightning@g.ucla.edu>
2024-08-26 11:06:56 -0700

    fix server

--- frontend/src/routes/+server.js
+++ frontend/src/routes/+server.js
@@ -13,30 +13,34 @@ import DonutsJDilla from '$lib/hard-coded_sample_reports/Donuts-J_Dilla.json'
 import ToPimpAButterflyKendrickLamar from '$lib/hard-coded_sample_reports/To_Pimp_a_Butterfly-Kendrick_Lamar.json'
 import ThreeFeetHighAndRisingDeLaSoul from '$lib/hard-coded_sample_reports/3_Feet_High_and_Rising-De_La_Soul.json'
 import PaulsBoutiqueBeastieBoys from '$lib/hard-coded_sample_reports/Pauls_Boutique-Beastie_Boys.json'
+import { json } from '@sveltejs/kit'
 
 export function GET({ url }) {
     console.log('got a request', url.searchParams.get('uri'))
     switch(url.searchParams.get('uri')) {
         case 'open.spotify.com/album/2noRn2Aes5aoNVsU6iWThc':
-            return new Response(JSON.stringify(DiscoveryDaftPunk))
+            return json(DiscoveryDaftPunk)
         case 'open.spotify.com/album/3DGQ1iZ9XKUQxAUWjfC34w':
             return new Response(JSON.stringify(goodkidmAAdcityKendrickLamar))
         case 'open.spotify.com/album/43uErencdmuTRFZPG3zXL1':
-            return new Response(JSON.stringify(PinataFreddieGibbs))
+            return json(PinataFreddieGibbs)
         case 'open.spotify.com/album/19bQiwEKhXUBJWY6oV3KZk':
             console.log('let me give you Madvillany')
-            return new Response(JSON.stringify(MadvillanyMFDoom))
+            return json(MadvillanyMFDoom)
         case 'open.spotify.com/album/3u20OXh03DjCUzbf8XcGTq':
-            return new Response(JSON.stringify(SCARINGTHEHOES_JPEGMAFIA))
+            return json(SCARINGTHEHOES_JPEGMAFIA)
         case 'open.spotify.com/album/1ezs1QD5SYQ6LtxpC9y5I2':
-            return new Response(JSON.stringify(ILAYDOWNMYLIFEFORYOU_JPEGMAFIA))
+            return json(ILAYDOWNMYLIFEFORYOU_JPEGMAFIA)
         case 'open.spotify.com/album/5fMlysqhFE0itGn4KezMBW':
-            return new Response(JSON.stringify(DonutsJDilla))
-        case 'https://open.spotify.com/album/7ycBtnsMtyVbbwTfJwRjSP':
-            return new Response(JSON.stringify(ToPimpAButterflyKendrickLamar))
-        case 'https://open.spotify.com/album/34LxHI9x14qXUOS8AWRrYD':
-            return new Response(JSON.stringify(ThreeFeetHighAndRisingDeLaSoul))
-        case 'https://open.spotify.com/album/1kmyirVya5fRxdjsPFDM05':
-            return new Response(JSON.stringify(PaulsBoutiqueBeastieBoys))
+            return json(DonutsJDilla)
+        case 'open.spotify.com/album/7ycBtnsMtyVbbwTfJwRjSP':
+            return json(ToPimpAButterflyKendrickLamar)
+        case 'open.spotify.com/album/34LxHI9x14qXUOS8AWRrYD':
+            return json(ThreeFeetHighAndRisingDeLaSoul)
+        case 'open.spotify.com/album/1kmyirVya5fRxdjsPFDM05':
+            return json(PaulsBoutiqueBeastieBoys)
+        default:
+            console.log('something went wrong')
+            return new Response(null, {status: 404})
     }
 }
