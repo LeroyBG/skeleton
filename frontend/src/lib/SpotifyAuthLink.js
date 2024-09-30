7590280ec668a397c35a240d8827fc6f7b0e3ee5
LeroyBG <leroylightning@g.ucla.edu>
2024-09-30 12:21:53 -0700

    remove unnecessary file for demo

--- frontend/src/lib/SpotifyAuthLink.js
+++
@@ -1,8 +0,0 @@
-import { PUBLIC_CLIENT_ID, PUBLIC_REDIRECT_URI } from "$env/static/public";
-export const authorizationRequestLink = 'https://accounts.spotify.com/authorize?'
-    + new URLSearchParams({
-        "client_id": PUBLIC_CLIENT_ID,
-        "response_type": "code",
-        "redirect_uri": PUBLIC_REDIRECT_URI,
-        "scope": "playlist-read-private playlist-modify-private playlist-modify-public"
-    }).toString()
